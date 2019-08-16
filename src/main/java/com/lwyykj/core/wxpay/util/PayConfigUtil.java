bfbd73f4a37539c35cfeff56273517b167af04f1
YangLi <525455531@qq.com>
2019-08-16 19:37:14 +0800

    Update PayConfigUtil.java

--- src/main/java/com/lwyykj/core/wxpay/util/PayConfigUtil.java
+++ src/main/java/com/lwyykj/core/wxpay/util/PayConfigUtil.java
@@ -5,9 +5,9 @@ import javax.servlet.http.HttpServletRequest;
 import com.lwyykj.common.Constants;
 
 public class PayConfigUtil {
-	public static final String APP_ID = "wx78edd96baf6e1873";// 微信开发平台应用ID(公众号ID)
-	public static final String MCH_ID = "1501213721";// 商户号(商户号ID)
-	public static final String API_KEY = "c15c86cd3b0abd5e75669eb1254040a1";// API key(商户号里面的)
+	public static final String APP_ID = "xxxxxx";// 微信开发平台应用ID(公众号ID)
+	public static final String MCH_ID = "xxxx";// 商户号(商户号ID)
+	public static final String API_KEY = "c15xxxxxxxx";// API key(商户号里面的)
 	public static final String CREATE_IP = "";// 发起支付的ip
 	public static final String NOTIFY_URL = Constants.DOMAIN+"/weixinpay/notify_url";// 回调地址
 	public static final String UFDODER_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";// 微信统一下单接口
